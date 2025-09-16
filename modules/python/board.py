3a294d373c016827e173b6da818b3283a6ba79a2
Phil Howard <github@gadgetoid.com>
2025-09-16 11:04:07 +0100

    Organise modules to specific and common.

--- modules/python/board.py
+++
@@ -1,5 +0,0 @@
-import machine
-
-# Pull all of the machine.Pin.board.LABEL definitions into the local scope
-# Note The firmware (via powman startup) will guarantee these are configured.
-locals().update((k,v) for (k,v) in machine.Pin.board.__dict__.items() if not k.startswith("_"))
