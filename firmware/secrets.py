f76b92690799b9d8a0536353fd0dc3e5e52a760e
thirdr <ryan@pimoroni.com>
2026-02-12 15:33:24 +0000

    firmware: added lat and lon to secrets.py for weather app

--- firmware/secrets.py
+++ firmware/secrets.py
@@ -2,3 +2,5 @@ WIFI_SSID = ""
 WIFI_PASSWORD = ""
 REGION = "eu"  # Options are us, cuba, eu, moldova, lebanon, egypt, chile, australia, nz
 TIMEZONE = 0  # Offset from GMT as number of hours, i.e. 0, 1, -7 etc.
+LAT = ""
+LON = ""
