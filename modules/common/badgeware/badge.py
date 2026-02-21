cac8bfe457d63aca024491c36e350c9091fabee8
Phil Howard <github@gadgetoid.com>
2026-02-21 18:40:18 +0000

    badge: Add clear and update methods.

--- modules/common/badgeware/badge.py
+++ modules/common/badgeware/badge.py
@@ -60,9 +60,6 @@ class Badge():
         # current display mode
         self._current_mode = None
 
-        # optional replacement update() function
-        self.update = None
-
         # either badger, tufty, or blinky
         self.model = MODEL
 
@@ -99,6 +96,19 @@ class Badge():
     @property
     def resolution(self):
         return screen.width, screen.height
+
+    def clear(self):
+        if self.default_clear is not None:
+            screen.pen = self.default_clear
+            screen.clear()
+        screen.pen = self.default_pen
+        return True
+
+    def update(self):
+        display.update()
+        badge.clear()
+        badge.poll()
+        return True
 
     def mode(self, mode=None):
         if mode is None:
